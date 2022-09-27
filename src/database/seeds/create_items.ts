fedb501cbee242b897c15ff026a1ec9055e5305e
Ignacio-antonio <yignaciosd@gmail.com>
2022-09-27 17:22:27 -0300

    feat: add table image in SQL

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -5,8 +5,8 @@ export async function seed(knex: Knex) {
         { title: 'Lâmpadas', image: 'lampadas.svg' },
         { title: 'Pilhas e Baterias', image: 'baterias.svg' },
         { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
-        { title: 'Resíduos Eletrônicos', image: 'eletrônicos.svg' },
-        { title: 'Resíduos Orgânicos', image: 'orgnânicos.svg' },
+        { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+        { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
         { title: 'Ôleo de Cozinha', image: 'oleo.svg'},
     ]);
 }
